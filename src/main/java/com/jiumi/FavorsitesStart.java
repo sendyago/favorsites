bb484e99867d85f035ed955538d0d9e5cebb50c9
zolosay <sendyago@live.com>
2025-09-06 23:14:47 +0800

    前后端框架首次上传

---
+++ src/main/java/com/jiumi/FavorsitesStart.java
@@ -0,0 +1,12 @@
+package com.jiumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorsitesStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavorsitesStart.class, args);
+    }
+}
